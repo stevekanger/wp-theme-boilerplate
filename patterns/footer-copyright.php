d87131a788150141518a0bf548170951ef3bdc05
stevekanger <dev.stevekanger@gmail.com>
2026-08-17 19:54:40 -0500

    feat: streamline templates and add footer-copyright and page-content-wrapper patterns

---
+++ patterns/footer-copyright.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Footer copyright
+ * Slug: wp-plugin-boilerplate/site-copyright
+ * Description: Paragraph with the site's copyright and site name
+ * Categories: text
+ * Keywords: footer, copyright
+ */
+?>
+
+<!-- wp:paragraph {"style":{"typography":{"textAlign":"center"}}} -->
+<p>&copy;<?php bloginfo('name'); ?> All Rights Reserved.</p>
+<!-- /wp:paragraph -->
